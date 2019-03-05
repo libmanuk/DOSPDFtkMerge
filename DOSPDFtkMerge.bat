6c11b3a1b2da4cb2c333c6d6ae23a19349e2d960
Eric C. Weig <eweig@email.uky.edu>
2019-03-05 13:06:54 -0500

    Update DOSPDFtkMerge.bat

--- DOSPDFtkMerge.bat
+++ DOSPDFtkMerge.bat
@@ -1,10 +1,12 @@
 echo off
 title DOSPDFtkMerge, Eric C. Weig 2019
 echo Hello, would you like to merge some PDFs?
-set /P ofn=Enter file name for merged PDF: 
+set /P ofn=Enter file name for merged PDF:
+set /P ext=Enter file extension for source files: 
 echo Using PDFtk for Windows to merge a directory of PDF files
 
-pdftk *.pdf cat output %ofn%.pdf
+pdftk *.%ext% cat output %ofn%.pdf
+
 echo.
 echo All done!  :)
 pause
