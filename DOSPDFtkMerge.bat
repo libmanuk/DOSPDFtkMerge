4ef8647705e274864b11803272971472a03f4352
Eric C. Weig <eweig@email.uky.edu>
2019-03-05 13:29:40 -0500

    Update DOSPDFtkMerge.bat

--- DOSPDFtkMerge.bat
+++ DOSPDFtkMerge.bat
@@ -5,7 +5,12 @@ set /P ofn=Enter file name for merged PDF:
 set /P ext=Enter file extension for source files: 
 echo Using PDFtk for Windows to merge a directory of PDF files
 
-pdftk *.%ext% cat output %ofn%.pdf
+for %%i in (*.%ext%) do echo %%i
+echo processing files, please wait...
+for %%i in (*.%ext%) do magick convert %%~ni.%ext% %%~ni.pdf
+echo.
+
+pdftk *.pdf cat output %ofn%.pdf
 
 echo.
 echo All done!  :)
