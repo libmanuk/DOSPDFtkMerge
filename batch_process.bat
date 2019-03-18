bcddf6e9d3b738bc06b73262a00af0c92df9aa36
Eric C. Weig <eweig@email.uky.edu>
2019-03-18 11:08:56 -0400

    Create batch_process.bat

---
+++ batch_process.bat
@@ -0,0 +1,8 @@
+echo off
+
+call DOSPDFtkMerge_m.bat merged_pdf_filename file_ext directory_to_process
+call DOSPDFtkMerge_m.bat merged_pdf_filename file_ext directory_to_process
+
+echo All batch processes completed.
+pause
+
