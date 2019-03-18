0eb7b33caad1573e6f8b881f1392776a0c4483ef
Eric C. Weig <eweig@email.uky.edu>
2019-03-18 15:33:42 -0400

    Update batch_process.bat

--- batch_process.bat
+++ batch_process.bat
@@ -1,8 +1,14 @@
 echo off
+rem   ****************************************
+title DOSPDFtkMerge, Eric C. Weig 2019
+rem   ****************************************
+rem   edit START
 
 call DOSPDFtkMerge_m.bat merged_pdf_filename file_ext directory_to_process
 call DOSPDFtkMerge_m.bat merged_pdf_filename file_ext directory_to_process
 
+rem  edit STOP
+echo.
 echo All batch processes completed.
 pause
 
